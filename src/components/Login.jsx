d041a464888e527a518c3d9217ec45139220e41e
Arindam Kundu <arindamkundu2002@gmail.com>
2026-07-22 07:04:17 +0530

    Stage 1/basic complete

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,9 +9,32 @@ const Login = () => {
 
     const [emailId, setEmailId] = useState("aakk2002+1@gmail.com");
     const [password, setPassword] = useState("aA!2aaaa");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [isLoginForm, setIsLoginForm] = useState(true);
+
     const dispatch = useDispatch();
+
     const navigate = useNavigate();
     const [error, setError] = useState("")
+
+
+const handleSignUp = async () => {
+    try {
+
+        const res = await axios.post(BASE_URL + "/signup", {firstName, lastName, emailId, password},
+            {withCredentials: true}
+        );
+        dispatch(addUser(res.data.data));
+        console.log(res.data.data)
+        return navigate("/profile")
+        
+    }  catch(err){
+        console.error(err.response)
+        setError(err.response.data);
+    }
+}
+
 
 
 
@@ -41,7 +64,33 @@ const Login = () => {
         <div className='flex justify-center my-10'>
             <div className="card bg-base-200 w-96 shadow-sm">
                 <div className="card-body">
-                    <h2 className="card-title text-2xl justify-center">Login</h2>
+                    <h2 className="card-title text-2xl justify-center">{isLoginForm ? "Login" : "SignUp"}</h2>
+                    {!isLoginForm && 
+                        (<>
+                    <fieldset className="fieldset">
+                        <legend className="fieldset">First Name</legend>
+
+                        <input type="text"
+                            value={firstName}
+                            className="input"
+                            placeholder="Type here"
+                            onChange={(e) => setFirstName(e.target.value)}
+                        />
+
+                    </fieldset>
+                    <fieldset className="fieldset">
+                        <legend className="fieldset">Last Name</legend>
+
+                        <input type="text"
+                            value={lastName}
+                            className="input"
+                            placeholder="Type here"
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
+
+                    </fieldset>
+                    </>
+                    )}
                     <fieldset className="fieldset">
                         <legend className="fieldset">Email Id</legend>
 
@@ -69,10 +118,13 @@ const Login = () => {
                     <p className='text-red-400'>{error}</p>
                     <div className="card-actions justify-center">
                         <button className="btn btn-primary"
-                        onClick={handleLogin}
-                        >Login
+                        onClick={isLoginForm ? handleLogin : handleSignUp}
+                        >
+                            {isLoginForm ? "Login" : "SignUp"}
                         </button>
                     </div>
+                            <p className='m-auto text-lime-400' onClick={()=>setIsLoginForm((value)=> !value)} >{isLoginForm ? "New User? SignUp here" : "Existing user? Login here"}</p>
+
                 </div>
             </div>
         </div>
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch } from 'react-redux';
+import { removeUserFromFeed } from '../utils/feedSlice';
+import axios from 'axios';
 
 const UserCard = ({ user }) => {
-    const { firstName, lastName, photoUrl, age, gender, about } = user;
+    const { _id, firstName, lastName, photoUrl, age, gender, about } = user;
+
+    const dispatch = useDispatch();
+
+    const handleRequest = async (status, userId) => {
+
+        try {
+            const res = await axios.post(BASE_URL + "/request/send/" + status + "/" + userId, {},
+                { withCredentials: true }
+            );
+
+            dispatch(removeUserFromFeed(userId));
+
+        } catch (error) {
+            console.log(error.response.data.message);
+
+        }
+    }
 
     console.log(user)
     return (
@@ -20,8 +41,14 @@ const UserCard = ({ user }) => {
                         {about}
                     </p>
                     <div className="card-actions justify-center">
-                        <button className="btn btn-primary">Sent Request</button>
-                        <button className="btn btn-secondary">Ignore</button>
+                        <button className="btn btn-primary"
+                        onClick={() => handleRequest("interested", _id)}
+                        >Sent Request
+                        </button>
+                        <button className="btn btn-secondary"
+                            onClick={() => handleRequest("ignored", _id)}>
+                            Ignore
+                        </button>
                     </div>
                 </div>
             </div>
