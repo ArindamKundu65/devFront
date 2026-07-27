869972ecaf6adee993eb99f5ec3b50733221174c
Arindam Kundu <arindamkundu2002@gmail.com>
2026-07-27 17:51:16 +0530

    Update project

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Premium = () => {
   return (
-    <div>Premium</div>
+    <div className="flex w-full">
+  <div className="card bg-base-300 rounded-box grid h-20 grow place-items-center">content</div>
+  <div className="divider divider-horizontal">OR</div>
+  <div className="card bg-base-300 rounded-box grid h-20 grow place-items-center">content</div>
+</div>
   )
 }
 
