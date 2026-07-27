76c236cb2310ad4eed8afa97f0af2c9fd664873e
Arindam Kundu <arindamkundu2002@gmail.com>
2026-07-27 22:35:03 +0530

    Update project

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,6 +1,43 @@
+import axios from 'axios'
 import React from 'react'
+import { BASE_URL } from '../utils/constants'
 
 const Premium = () => {
+    const handleBuyClick = async (type) => {
+
+
+        const order = await axios.post( BASE_URL + "/payment/create", {
+            memberShipType: type
+        }, { withCredentials: true })
+
+    };
+
+    const { amount, keyId, currency, notes, orderId } = order.data;
+
+    const options = {
+        key: keyId, 
+        amount,
+        currency,
+        name: 'Dev Corner',
+        description: 'Connect to other developers',
+        order_id: orderId,
+        prefill: {
+          name: notes.firstName + ' ' + notes.lastName,
+          email: notes.emailId,
+          contact: '9999999999'
+        },
+        theme: {
+          color: '#AAFF00'
+        },
+      };
+
+
+    const rzp = new Razorpay(options);
+    rzp.open();
+
+
+
+
     return (
         <div className="flex w-full mt-10">
             <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
@@ -11,7 +48,7 @@ const Premium = () => {
                     <li> - Blue Tick</li>
                     <li> - 3 months</li>
                 </ul>
-                <button className='btn bg-gray-400 hover:bg-gray-500'>Buy Sliver</button>
+                <button onClick={()=>handleBuyClick("silver")} className='btn bg-gray-400 hover:bg-gray-500'>Buy Sliver</button>
             </div>
             <div className="divider divider-horizontal">OR</div>
             <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
@@ -22,7 +59,7 @@ const Premium = () => {
                     <li> - Blue Tick</li>
                     <li> - 6 months</li>
                 </ul>
-                <button className='btn btn-warning'>Buy Gold</button>
+                <button onClick={()=>handleBuyClick("gold")} className='btn btn-warning'>Buy Gold</button>
             </div>
         </div>
     )
