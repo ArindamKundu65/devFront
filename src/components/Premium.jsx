bb9a7e95884f65437354362bc73ea0d5b2bb0aee
Arindam Kundu <arindamkundu2002@gmail.com>
2026-07-27 18:12:25 +0530

    Update project

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 
 const Premium = () => {
-  return (
-    <div className="flex w-full mt-10">
-  <div className="card bg-base-300 rounded-box grid h-20 grow place-items-center">content</div>
-  <div className="divider divider-horizontal">OR</div>
-  <div className="card bg-base-300 rounded-box grid h-20 grow place-items-center">content</div>
-</div>
-  )
+    return (
+        <div className="flex w-full mt-10">
+            <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
+                <h1 className='font-bold text-3xl'>Silver Membership</h1>
+                <ul>
+                    <li> - Chat with other people</li>
+                    <li> - 100 connection requests per day</li>
+                    <li> - Blue Tick</li>
+                    <li> - 3 months</li>
+                </ul>
+                <button className='btn-secondary'>Buy Sliver</button>
+            </div>
+            <div className="divider divider-horizontal">OR</div>
+            <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
+                <h1 className='font-bold text-3xl'>Gold Membership</h1>
+                <ul>
+                    <li> - Chat with other people</li>
+                    <li> - Infinite connection requests per day</li>
+                    <li> - Blue Tick</li>
+                    <li> - 6 months</li>
+                </ul>
+                <button className='btn-primary'>Buy Gold</button>
+            </div>
+        </div>
+    )
 }
 
 export default Premium
