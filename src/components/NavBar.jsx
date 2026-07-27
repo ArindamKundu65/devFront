c0abdd88c626f08a6626c37a1da9664aedd65c96
Arindam Kundu <arindamkundu2002@gmail.com>
2026-07-27 17:17:39 +0530

    Fix CORS and email configuration

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -67,7 +67,7 @@ const NavBar = () => {
             <Link to={"/requests"}>Requests</Link>
             </li>
             <li>
-              <Link to={"/premium"}></Link>
+              <Link to={"/premium"}>Premium</Link>
             </li>
           <li><a onClick={handleLogout}>Logout</a></li>
         </ul>
