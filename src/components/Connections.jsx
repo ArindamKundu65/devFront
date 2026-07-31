47f32bb01e485a7d03972f2b61cfae06c31d4ec1
Arindam Kundu <arindamkundu2002@gmail.com>
2026-07-31 22:45:15 +0530

    d

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -14,7 +14,8 @@ const Connections = () => {
             const res = await axios.get(BASE_URL + "/user/connections", {
                 withCredentials: true,
             });
-            console.log(res?.data?.data);
+            console.log("res.data =", res.data);
+            console.log("res.data.data =", res.data.data);
             dispatch(addConnections(res.data.data));
 
         } catch (err) {
