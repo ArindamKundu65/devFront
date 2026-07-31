6b5eb788b260f4b3950f3145bd92c80304984bbb
Arindam Kundu <arindamkundu2002@gmail.com>
2026-07-31 21:42:09 +0530

    hasmore state

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,37 +1,60 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { BASE_URL } from '../utils/constants'
 import { useDispatch, useSelector } from 'react-redux'
 import { addFeed } from '../utils/feedSlice'
 import UserCard from './UserCard'
 
+
+
+
 const Feed = () => {
+
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+
   const feed = useSelector((store) => store.feed)
   const dispatch = useDispatch();
 
   const getFeed = async () => {
-    if (feed) return;
-    
-    try {const res = await axios.get(BASE_URL + "/feed",
-      {withCredentials: true}
-    );
-    dispatch(addFeed(res?.data));
-  }
-  catch (err) {
-    console.log(err.response?.status);
-    console.log(err.response?.data);
-  }
+    if (!hasMore) return;
+  
+    try {
+      const res = await axios.get(
+        `${BASE_URL}/feed?page=${page}&limit=10`,
+        {
+          withCredentials: true,
+        }
+      );
+  
+      if (res.data.length === 0) {
+        setHasMore(false);
+        return;
+      }
+  
+      dispatch(addFeed(res.data));
+    } catch (err) {
+      console.log(err.response?.status);
+      console.log(err.response?.data);
+    }
+  };
 
-  }
+  useEffect(() => {
+    getFeed();
+  }, [page]);
 
-  useEffect(()=>{
-    getFeed()
-  }, []);
+  useEffect(() => {
+    if (hasMore && feed && feed.length === 0) {
+      setPage((prev) => prev + 1);
+    }
+  }, [feed, hasMore]);
 
-  return (
+return (
   <div className="flex justify-center my-10">
     {feed && feed.length > 0 ? (
       <UserCard user={feed[0]} />
+    ) : hasMore ? (
+      <h2>Loading...</h2>
     ) : (
       <h2>No more users available</h2>
     )}
