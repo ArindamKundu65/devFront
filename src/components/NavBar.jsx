4ff610d841d01de4c29af2571fbda145a61d0c3d
Arindam Kundu <arindamkundu2002@gmail.com>
2026-07-31 19:47:23 +0530

    resetfeed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom';
 import { BASE_URL } from '../utils/constants';
 import { removeUser } from '../utils/userSlice';
+import { resetFeed } from '../utils/feedSlice';
 
 
 const NavBar = () => {
@@ -22,6 +23,8 @@ const NavBar = () => {
         {withCredentials: true}
       )
       dispatch(removeUser());
+      dispatch(resetFeed());
+      
       return navigate("/login")
       
     } catch (error) {
--- src/utils/feedSlice.js
+++ src/utils/feedSlice.js
@@ -1,19 +1,42 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const feedSlice = createSlice({
+//     name: "feed",
+//     initialState: null,
+//     reducers:{
+//         addFeed:(state, action) => {
+//             return action.payload;
+//         },
+//         removeUserFromFeed: (state, action) => {
+//             const newFeed = state.filter((user)=> user._id !== action.payload)
+//             return newFeed;
+//         }
+//     }
+// })
+
+
+// export const {addFeed, removeUserFromFeed} = feedSlice.actions;
+// export default feedSlice.reducer;
+
 import { createSlice } from "@reduxjs/toolkit";
 
 const feedSlice = createSlice({
-    name: "feed",
-    initialState: null,
-    reducers:{
-        addFeed:(state, action) => {
-            return action.payload;
-        },
-        removeUserFromFeed: (state, action) => {
-            const newFeed = state.filter((user)=> user._id !== action.payload)
-            return newFeed;
-        }
-    }
-})
+  name: "feed",
+  initialState: null,
+  reducers: {
+    addFeed: (state, action) => {
+      return action.payload;
+    },
 
+    removeUserFromFeed: (state, action) => {
+      return state.filter((user) => user._id !== action.payload);
+    },
 
-export const {addFeed, removeUserFromFeed} = feedSlice.actions;
+    resetFeed: () => {
+      return null;
+    },
+  },
+});
+
+export const { addFeed, removeUserFromFeed, resetFeed } = feedSlice.actions;
 export default feedSlice.reducer;
