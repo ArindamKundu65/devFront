cdb621d32eb91628162706eb3a1adab5c3f3384c
Arindam Kundu <arindamkundu2002@gmail.com>
2026-08-01 18:28:45 +0530

    f

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,7 +4,20 @@ import { useParams } from 'react-router-dom'
 const Chat = () => {
     const { targetUserId } = useParams();
   return (
-    <div>Chat</div>
+    <div className='w-1/2 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col'>
+      <h1 className='p-5 border-b border-gray-600'>Chat</h1>
+      <div>
+
+      </div>
+
+
+      <div>
+        <input className=''></input>
+        <button>Send</button>
+
+      </div>
+    </div>
+
   )
 }
 
