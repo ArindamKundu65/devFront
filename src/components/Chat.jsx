70c14686fc84d3c0f5b0a80c7e20cfe7478b2a65
Arindam Kundu <arindamkundu2002@gmail.com>
2026-08-01 21:52:56 +0530

    socket

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { createSocketConnection } from '../utils/socket';
+import { useSelector } from 'react-redux';
 
 const Chat = () => {
     const { targetUserId } = useParams();
     const [messages, setMessages] = useState([{text: "Hello World"}])
+    const user = useSelector(store => store.user);
+    const userId = user?._id;
+
+  useEffect(()=> {
+    const socket = createSocketConnection();
+    socket.emit("joinChat", { userId, targetUserId });
+
+    return () => {
+      socket.disconnect();
+    }
+  }, [])
+
   return (
     <div className='w-1/2 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col'>
       <h1 className='p-5 border-b border-gray-600'>Chat</h1>
@@ -28,7 +42,7 @@ const Chat = () => {
 
 
       <div className='p-5 border-t border-gray-600 flex items-center gap-2'>
-        <input className='flex-1 border border-gray-500 text-white rounded p-2'></input>
+        <input className='flex-1 border border-green-500 text-white rounded p-2'></input>
         <button className='btn btn-secondary'>Send</button>
 
       </div>
