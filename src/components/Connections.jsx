789101b090b317f5402f231323a1bbbfe2b197eb
Arindam Kundu <arindamkundu2002@gmail.com>
2026-08-01 18:14:19 +0530

    link

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import { BASE_URL } from '../utils/constants'
 import { useDispatch, useSelector } from 'react-redux'
 import { addConnections } from '../utils/connectionSlice'
+import { Link } from 'react-router-dom'
 
 const Connections = () => {
     const connections = useSelector((store) => store.connections);
@@ -19,6 +20,7 @@ const Connections = () => {
             dispatch(addConnections(res.data.data));
 
         } catch (err) {
+            console.error(err)
 
         }
     };
@@ -47,8 +49,9 @@ const Connections = () => {
                             {age && gender && <p>{age + " " + gender}</p>}
                             {<p>{skills}</p>}
                         </div>
+                        <Link to={"/chat/"+_id}>
                         <button className='btn btn-primary'>Chat</button>
-
+                        </Link>
                         
                     </div>
                 )
