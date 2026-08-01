431bae160a8967f62eab264d73d44f6daa7b71d1
Arindam Kundu <arindamkundu2002@gmail.com>
2026-08-01 18:49:59 +0530

    array

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 
 const Chat = () => {
     const { targetUserId } = useParams();
-    const {messages, setMessages} = useState({text: "Hello World"})
+    const {messages, setMessages} = useState([{text: "Hello World"}])
   return (
     <div className='w-1/2 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col'>
       <h1 className='p-5 border-b border-gray-600'>Chat</h1>
