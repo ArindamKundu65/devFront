a5985c226bfa40fe65548968ab50f192b6048fb7
Arindam Kundu <arindamkundu2002@gmail.com>
2026-08-01 18:37:28 +0530

    ff

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -12,8 +12,8 @@ const Chat = () => {
 
 
       <div className='p-5 border-t border-gray-600 flex items-center gap-2'>
-        <input className=''></input>
-        <button>Send</button>
+        <input className='flex-1 border border-gray-500 text-white rounded p-2'></input>
+        <button className='btn btn-secondary'>Send</button>
 
       </div>
     </div>
