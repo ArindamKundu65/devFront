e93b14c06b249b19b58110a67bb2547c507ca171
Arindam Kundu <arindamkundu2002@gmail.com>
2026-08-01 18:33:08 +0530

    flexx

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -6,7 +6,7 @@ const Chat = () => {
   return (
     <div className='w-1/2 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col'>
       <h1 className='p-5 border-b border-gray-600'>Chat</h1>
-      <div>
+      <div className='flex-1 overflow-scroll p-5'>
 
       </div>
 
