1b0ab3452dfa1bb0ef84f0ed0fab8650008aeaa6
Arindam Kundu <arindamkundu2002@gmail.com>
2026-08-02 20:22:45 +0530

    zz

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { createSocketConnection } from '../utils/socket';
 import { useSelector } from 'react-redux';
+import { Socket } from 'socket.io-client';
 
 const Chat = () => {
     const { targetUserId } = useParams();
     const [messages, setMessages] = useState([{text: "Hello World"}])
+    const [newMessage, setNewMessage] = useState("");
     const user = useSelector(store => store.user);
     const userId = user?._id;
 
@@ -22,21 +24,15 @@ const Chat = () => {
     };
   }, [userId, targetUserId])
 
-  useEffect(() => {
-    console.log("Chat mounted");
-  
-    const socket = createSocketConnection();
-  
-    socket.on("connect", () => {
-      console.log("Socket connected:", socket.id);
-    });
-  
-    socket.on("connect_error", (err) => {
-      console.log("Connect error:", err.message);
-    });
-  
-    return () => socket.disconnect();
-  }, []);
+const sendMessage = () => {
+  const socket = createSocketConnection();
+  socket.emit("sendMessage", {
+    firstName: user.firstName,
+    userId,
+    targetUserId,
+    text: newMessage
+  })
+}
 
   return (
     <div className='w-1/2 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col'>
@@ -62,8 +58,10 @@ const Chat = () => {
 
 
       <div className='p-5 border-t border-gray-600 flex items-center gap-2'>
-        <input className='flex-1 border border-green-500 text-white rounded p-2'></input>
-        <button className='btn btn-secondary'>Send</button>
+        <input value={newMessage}
+        onChange={(e)=> setNewMessage(e.target.value)}
+         className='flex-1 border border-green-500 text-white rounded p-2'></input>
+        <button onClick={sendMessage} className='btn btn-secondary'>Send</button>
 
       </div>
     </div>
